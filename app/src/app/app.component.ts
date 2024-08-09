f8e4127ccaa96a292fe4887461515e7cacebaf42
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-09 10:37:41 +0200

    Created empty about section with nav linkage

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { ProfileComponent } from "./profile/profile.component";
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, ProfileComponent],
+  imports: [RouterOutlet, NavigationComponent, ProfileComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
