1eb6f0dd2bd1e7252b3c23a7d2b10fe6b28629b9
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-14 10:15:59 +0200

    Added skill and experience on skill-content.component

--- app/src/app/skills/skills-container/skill-content/skill-content.component.ts
+++ app/src/app/skills/skills-container/skill-content/skill-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-skill-content',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './skill-content.component.css'
 })
 export class SkillContentComponent {
+
+  skill: string = 'Angular';
+  experience: string = 'Basic';
 
 }
