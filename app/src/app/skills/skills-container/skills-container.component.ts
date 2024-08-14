088766bb1d6b4f85b5ec00d4ef2b24c15f699d9a
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-14 10:55:42 +0200

    Finished logic of skills-container

--- app/src/app/skills/skills-container/skills-container.component.ts
+++ app/src/app/skills/skills-container/skills-container.component.ts
@@ -1,34 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SkillContentComponent } from "./skill-content/skill-content.component";
 import { NgFor } from '@angular/common';
 import { Skill } from '../Skill';
+import { HttpClientModule } from '@angular/common/http';
+import { SkillServiceService } from './skill-service.service';
 
 @Component({
   selector: 'app-skills-container',
   standalone: true,
-  imports: [SkillContentComponent, NgFor],
+  imports: [SkillContentComponent, NgFor, HttpClientModule],
   templateUrl: './skills-container.component.html',
   styleUrl: './skills-container.component.css'
 })
-export class SkillsContainerComponent {
+export class SkillsContainerComponent implements OnInit {
 
   // Programming Languages
   skillset1: Skill[] = [
-    new Skill('JavaScript', 'Intermediate'),
-    new Skill('TypeScript', 'Intermediate'),
-    new Skill('Python', 'Intermediate'),
-    new Skill('Java', 'Intermediate'),
-    new Skill('C#', 'Intermediate'),
-    new Skill('C++', 'Intermediate'),
-    new Skill('C', 'Intermediate'),
+
   ];
 
   // Web Frameworks
   skillset2: Skill[] = [
-    new Skill('Angular', 'Intermediate'),
-    new Skill('React', 'Intermediate'),
-    new Skill('Vue', 'Intermediate'),
-    new Skill('Node.js', 'Intermediate'),
+
   ];
+
+
+  constructor(private skillService: SkillServiceService) { }
+
+  ngOnInit() {
+    this.skillService.getSkillSet1().subscribe((data: Skill[]) => {
+      console.log(data);
+      this.skillset1 = data;
+    });
+
+    this.skillService.getSkillSet2().subscribe((data: Skill[]) => {
+      console.log(data);
+      this.skillset2 = data;
+    });
+  }
 
 }
