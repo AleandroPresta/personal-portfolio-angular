cd6e8b3592f6248bce0b3304718ebbc4e24fce42
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-16 14:45:15 +0200

    refactor: Add project tags to Project class and display them in project content component

--- app/src/app/projects/projects-list/project-content/project-content.component.ts
+++ app/src/app/projects/projects-list/project-content/project-content.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CustomBadgeComponent } from '../../../custom-badge/custom-badge.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-project-content',
   standalone: true,
-  imports: [CustomBadgeComponent],
+  imports: [CustomBadgeComponent, NgFor],
   templateUrl: './project-content.component.html',
   styleUrl: './project-content.component.css'
 })
@@ -14,5 +15,6 @@ export class ProjectContentComponent {
   @Input() projectDescription: string = 'Description 1';
   @Input() projectImageUrl: string = 'https://placehold.co/300x300/orange/white';
   @Input() projectUrl: string = 'https://www.google.com';
+  @Input() projectTags: string[] = ['Tag 1', 'Tag 2', 'Tag 3'];
 
 }
