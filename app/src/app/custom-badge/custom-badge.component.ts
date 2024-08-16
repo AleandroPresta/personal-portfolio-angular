0cd872923b63dd1cb9caf6f599e8fb164d38aee8
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-16 14:39:20 +0200

    chore: Update custom-badge component to use dynamic badge text

--- app/src/app/custom-badge/custom-badge.component.ts
+++ app/src/app/custom-badge/custom-badge.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-custom-badge',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './custom-badge.component.css'
 })
 export class CustomBadgeComponent {
+
+  @Input() badgeText: string = 'Badge Text';
 
 }
