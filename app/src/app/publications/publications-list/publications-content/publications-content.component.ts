fc8a36775c1815fa50313107af89066faff7234b
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-17 10:13:06 +0200

    Made projects fit the smallest screen

--- app/src/app/publications/publications-list/publications-content/publications-content.component.ts
+++ app/src/app/publications/publications-list/publications-content/publications-content.component.ts
@@ -1,21 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { Author } from '../../Publication';
 import { CustomBadgeComponent } from '../../../custom-badge/custom-badge.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-publications-content',
   standalone: true,
-  imports: [CustomBadgeComponent],
+  imports: [CustomBadgeComponent, NgFor],
   templateUrl: './publications-content.component.html',
   styleUrl: './publications-content.component.css'
 })
 export class PublicationsContentComponent {
 
-  @Input() publicationTitle: string = '';
-  @Input() publicationTags: string[] = [];
-  @Input() publicationPreviewImage: string = '';
-  @Input() publicationDate: string = '';
-  @Input() publicationAuthors: Author[] = [];
+  @Input() publicationTitle: string = 'Paper Title';
+  @Input() publicationTags: string[] = ['Tag 1', 'Tag 2', 'Tag 3'];
+  @Input() publicationPreviewImage: string = 'https://placehold.co/150x150';
+  @Input() publicationDate: string = 'Aug 2024';
+  @Input() publicationAuthors: Author[] = [
+    new Author('Author 1', 'https://placehold.co/25x25'),
+    new Author('Author 2', 'https://placehold.co/25x25'),
+    new Author('Author 3', 'https://placehold.co/25x25')
+  ];
 
   constructor() { }
 
